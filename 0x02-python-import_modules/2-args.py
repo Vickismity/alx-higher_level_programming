b39f05c26f1c18a34cf9446f515ae6578c276eef
Vickismity <ibshegs5000@gmail.com>
2024-01-21 13:44:54 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    n = len(argv) - 1
+    if n < 1:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument.".format(n))
+    else:
+        if n > 1:
+            print("{} arguments.".format(n))
+
+    for i in range(n):
+        print("{}: {:s}".format(i + 1, argv[i + 1]))
