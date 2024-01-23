90f72c365dec21c308c2306f2af869f64a7b327c
Vickismity <ibshegs5000@gmail.com>
2024-01-23 15:23:12 -0800

    11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!usr/bin/pyhton3
+def delete_at(my_list=[], idx=0):
+    length = len(my_list)
+    if idx >lenght - 1:
+        return (my_list)
+    elif idx < 0:
+        return (my_list)
+    del my_list[idx]
+    return (my_list)
