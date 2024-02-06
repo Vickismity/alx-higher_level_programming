3b90c57535235caec35a5ef32f634bee365eafef
Oluwasegun Ibraheem <ibshegs5000@gmail.com>
2024-02-05 19:09:39 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
-    for item in range(x):
+    for items in range(x):
         try:
-            print("{:d}".format(my_list[item]), end='')
+            print("{:d}".format(my_list[items]), end='')
             num += 1
         except(ValueError, TypeError):
             pass
