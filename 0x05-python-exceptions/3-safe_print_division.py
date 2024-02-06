70930b116322a1a3ecb9f3076ab82d7c1afe671f
Oluwasegun Ibraheem <ibshegs5000@gmail.com>
2024-02-05 19:22:49 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def safe_print_division(a, b):
+    try:
+        div = a/b
+    except(ValueError, TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+        return div
